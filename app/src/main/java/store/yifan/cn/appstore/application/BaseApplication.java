a07cc6628c6cd675560d716635f288cee26b66fb
jwfksky <472401736@qq.com>
2015-04-14 15:49:48 +0800

    add utils

---
+++ app/src/main/java/store/yifan/cn/appstore/application/BaseApplication.java
@@ -0,0 +1,14 @@
+package store.yifan.cn.appstore.application;
+
+import android.app.Application;
+
+/**
+ * Author: Jwf(feijia101@gmail.com) <br\>
+ * Date: 2015-04-14 15:45<br\>
+ * Version: 1.0<br\>
+ * Desc:<br\>
+ * Revise:<br\>
+ */
+public class BaseApplication extends Application {
+
+}
