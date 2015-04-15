111bd9a0f3e8e4da7c06dc5307bac64d769f5f97
jwfksky <472401736@qq.com>
2015-04-15 14:59:44 +0800

    init

---
+++ app/src/main/java/store/yifan/cn/appstore/fragment/AppFragment.java
@@ -0,0 +1,11 @@
+package store.yifan.cn.appstore.fragment;
+
+/**
+ * Author: Jwf(feijia101@gmail.com) <br\>
+ * Date: 2015-04-15 14:52<br\>
+ * Version: 1.0<br\>
+ * Desc:<br\>
+ * Revise:<br\>
+ */
+public class AppFragment extends BaseFragment {
+}
---
+++ app/src/main/java/store/yifan/cn/appstore/fragment/BaseFragment.java
@@ -0,0 +1,14 @@
+package store.yifan.cn.appstore.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Author: Jwf(feijia101@gmail.com) <br\>
+ * Date: 2015-04-15 14:26<br\>
+ * Version: 1.0<br\>
+ * Desc:<br\>
+ * Revise:<br\>
+ */
+public class BaseFragment extends Fragment {
+
+}
